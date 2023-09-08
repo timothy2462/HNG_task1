3f661745224a43854712bd92df161860edbfc82a
Timothy Akobundu <timothy.christian10@gmail.com>
2023-09-08 16:52:30 +0100

    added a new data attribute to my slack name

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 // Logic to get the day of the week
-const current_day_week = document.getElementById('dayWeek')
+const current_day_week = document.getElementById("dayWeek");
 
 const date = new Date();
 
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const dayOfWeek = daysOfWeek[date.getDay()];
 
-current_day_week.innerHTML = dayOfWeek
-
+current_day_week.innerHTML = dayOfWeek;
 
 // displaying the UTC in milliseconds
-const UTC_time = document.getElementById('utcTime')
+const UTC_time = document.getElementById("utcTime");
 const currentUTCMilliseconds = new Date().getTime();
-UTC_time.innerHTML= `${new Date(currentUTCMilliseconds).toLocaleString()}  ${currentUTCMilliseconds}`
+UTC_time.innerHTML = `${new Date(
+  currentUTCMilliseconds
+).toLocaleString()}  ${currentUTCMilliseconds}`;
